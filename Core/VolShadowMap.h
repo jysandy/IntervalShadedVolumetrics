47fa56064e009493052506514463e6ea67081bd0
Sandilya Jandhyala <jysandilya@gmail.com>
2025-07-29 17:25:39 +0100

    Increase shadow map depth slices

--- Core/VolShadowMap.h
+++ Core/VolShadowMap.h
@@ -12,7 +12,7 @@ namespace ISV
     {
     public:
         const uint32_t Width = 256;
-        const uint32_t Depth = 5;
+        const uint32_t Depth = 10;
 
         using DrawFn = std::function<void(DirectX::SimpleMath::Matrix,
             DirectX::SimpleMath::Matrix, DirectX::BoundingOrientedBox, float)>;

db94e00d67053ea16a7c8087d0a71a7678f06f01
Sandilya Jandhyala <jysandilya@gmail.com>
2026-01-06 15:13:50 +0530

    Make a swirling vortex animation

--- Shaders/SimulateParticles_CS.hlsl
+++ Shaders/SimulateParticles_CS.hlsl
@@ -1,4 +1,5 @@
 #include "TetrahedronPipeline.hlsli"
+#include "Quaternion.hlsli"
 
 RWStructuredBuffer<InstanceData> Instances : register(u0, space0);
 
@@ -10,17 +11,30 @@ float3 LineToPoint(float3 lineStart, float3 lineDirection, float3 p)
     return -pointToLine;
 }
 
+float3 GetLocalTargetPosition(uint3 DTid)
+{
+    float angularVelocity = 3.f + 100.f * frac(353435.22425 * DTid.x);
+    float angle = angularVelocity * g_totalTime;
+    
+    Quaternion rotationQuat = QuatFromAxisAngle(float3(0, 1, 0), angle);
+    float3 targetPosition = mul(Instances[DTid.x].TargetPosition, QuatTo3x3(rotationQuat)) 
+        * (1 + 3.xxx * sin(3 * g_totalTime + DTid.x));
+    
+    return targetPosition;
+}
+
 [numthreads(32, 1, 1)]
 void SimulateParticles_CS(uint3 DTid : SV_DispatchThreadID)
 {
     float3 worldPosition = Instances[DTid.x].WorldPosition;
-    float3 targetPosition = mul(float4(Instances[DTid.x].TargetPosition, 1), g_TargetWorld)
+    
+    float3 targetPosition = mul(float4(GetLocalTargetPosition(DTid), 1), g_TargetWorld)
         .xyz;
     
     float3 attractionVector = targetPosition - worldPosition;
     
     // Like gravity, this is independent of mass
-    float3 attractionAcceleration = 50.f * 
+    float3 attractionAcceleration = 150.f * 
         normalize(targetPosition - worldPosition);
     
     if (length(attractionVector) < 0.0001)
